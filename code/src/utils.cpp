bdf8b18d88bb8dc7631ebf082860aa54d309b1ef
Nathan Perkins <nperkins487@gmail.com>
2018-06-03 12:33:40 -0700

    add distance calculation

--- code/src/utils.cpp
+++ code/src/utils.cpp
@@ -1,7 +1,13 @@
 #include "utils.hpp"
+#include <cmath>
+
+int vertex_distance(struct vertex *start, struct vertex *end) {
+    return round(sqrt((start->x - end->x)^2 + (start->y - end->y)^2));
+}
 
 struct tsp_problem read_problem(char* in_file) {
     struct tsp_problem new_problem;
+
 
 
     return new_problem;
