193ecb847bcfc73478fa7eb3591851b263247427
Nathan Perkins <nathan.perkins@ubisoft.com>
2018-06-03 12:11:10 -0700

    read in filename argument from command-line

--- code/tsp.cpp
+++ code/tsp.cpp
@@ -1,3 +1,18 @@
+#include <stdio.h>
+
+#include "src/2-opt.hpp"
+#include "src/nearest_neighbor.hpp"
+#include "src/utils.hpp"
+
 int main(int argc, char** argv) {
+    if( argc < 2 ) {
+        printf("Need 1 argument: [input filename]\n");
+        return 1;
+    }
+
+    char* in_file = argv[1];
+
+    printf("Reading file: %s\n", in_file);
+
     return 0;
 }
